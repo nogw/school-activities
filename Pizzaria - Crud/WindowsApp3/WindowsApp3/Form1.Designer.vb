2c5b910d6fa83e1f2449db8204cbde9adb1d12c8
nogw <gabrielnogueiraoliveira@gmail.com>
2021-11-04 21:51:02 -0300

    wip

---
+++ Pizzaria - Crud/WindowsApp3/WindowsApp3/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(299, 28)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(475, 394)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 233)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(281, 26)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Pedir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.CheckOnClick = True
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"Pizza de Mussarela", "Pizza de Pepperone", "Pizza de Calabresa", "Pizza de Portuguesa", "Pizza de Alho"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(12, 28)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(281, 79)
+        Me.CheckedListBox1.TabIndex = 4
+        '
+        'CheckedListBox2
+        '
+        Me.CheckedListBox2.CheckOnClick = True
+        Me.CheckedListBox2.FormattingEnabled = True
+        Me.CheckedListBox2.Items.AddRange(New Object() {"Dolly 1 Litro", "Guarana 1 Litro", "Suco de laranja 1 Litro", "Pepsi 1 Litro", "Itubaina 1 Litro"})
+        Me.CheckedListBox2.Location = New System.Drawing.Point(12, 136)
+        Me.CheckedListBox2.Name = "CheckedListBox2"
+        Me.CheckedListBox2.Size = New System.Drawing.Size(281, 79)
+        Me.CheckedListBox2.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Sabores"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Bebidas"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 265)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(281, 26)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Listar pedidos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckedListBox2)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Pizzaria "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents CheckedListBox2 As CheckedListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Pizzaria - Crud/WindowsApp3/WindowsApp3/Form1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Const ConnectionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nogueira\Desktop\WindowsApp3\WindowsApp3\WindowsApp3\Pizzaria.mdf; Integrated Security=True; Connect Timeout=30"
+    Dim myConn As New SqlConnection(ConnectionString)
+    Dim myCmd As SqlCommand
+    Dim myAdapter As New SqlDataAdapter
+    Dim myDataTable As DataTable
+
+    Public Class GlobalVariables
+        Public Shared s = New List(Of String)
+    End Class
+
+    Private Sub PizzariaLoad(send As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            myConn.Open()
+            myCmd = New SqlCommand With {
+                .Connection = myConn,
+                .CommandText = "CREATE DATABASE pedidos"
+            }
+            myCmd.ExecuteNonQuery()
+
+            myCmd = New SqlCommand With {
+                .Connection = myConn,
+                .CommandText = "
+                CREATE TABLE IF NOT EXISTS pedidos ( 
+                    user_id uuid PRIMARY KEY NOT NULL
+                    orders VARCHAR 
+                )"
+            }
+            myCmd.ExecuteNonQuery()
+            myCmd.Dispose()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            myConn.Close()
+        End Try
+
+    End Sub
+
+    Private Function SaveFlavorList(ByVal User As String, ByVal Orders As String) As Boolean
+        Try
+            myConn.Open()
+            myCmd = New SqlCommand With {
+                .Connection = myConn,
+                .CommandText = "
+                INSERT INTO PEDIDOS (User_id, Orders) 
+                VALUES(@User, @Orders)"
+            }
+            myCmd.Parameters.Add("@User", SqlDbType.VarChar)
+            myCmd.Parameters("@User").Value = User
+            myCmd.Parameters.Add("@Orders", SqlDbType.VarChar)
+            myCmd.Parameters("@Orders").Value = Orders
+            myCmd.ExecuteNonQuery()
+            myConn.Close()
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Function GetFlavorList(ByVal User As String) As DataTable
+        myConn.Open()
+        myCmd = New SqlClient.SqlCommand With {
+            .Connection = myConn,
+            .CommandText = "
+                SELECT orders
+                FROM pedidos
+                WHERE user_id = @User
+            "
+        }
+        myCmd.Parameters.Add("@User", SqlDbType.VarChar)
+        myCmd.Parameters("@User").Value = User
+        myAdapter.SelectCommand = myCmd
+        myAdapter.Fill(myDataTable)
+        Dim Result As New DataTable
+        myConn.Close()
+        Return Result
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GlobalVariables.s.Clear()
+        ListBox1.Items.Clear()
+
+        For x = 0 To CheckedListBox1.CheckedItems.Count - 1
+            GlobalVariables.s.Add(CheckedListBox1.CheckedItems(x).ToString)
+        Next x
+
+        For x = 0 To CheckedListBox2.CheckedItems.Count - 1
+            GlobalVariables.s.Add(CheckedListBox2.CheckedItems(x).ToString)
+        Next x
+
+        Dim value As String = String.Join(",", GlobalVariables.s)
+
+        SaveFlavorList("023g3a2", value)
+
+        For x = 0 To GlobalVariables.s.Count - 1
+            ListBox1.Items.Add(GlobalVariables.s(x))
+        Next x
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        GetFlavorList("023g3a2")
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub CheckedListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox2.SelectedIndexChanged
+
+    End Sub
+End Class
